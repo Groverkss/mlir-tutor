8d0fd7aba8d6611764f929650a320193e2a46d4a
Kunwar Grover <groverkss@gmail.com>
2026-01-15 16:00:13 +0000

    Init

---
+++ README.md
@@ -0,0 +1,34 @@
+## Installation Instructions
+
+1. Create a virtual environment and activate it:
+
+```bash
+python3 -m venv venv
+source venv/bin/activate
+```
+
+2. Install required packages:
+
+```bash
+pip install -r requirements.txt
+```
+
+3. Check if the installation was successful:
+
+```bash
+python3 -m mlir_wheel --root-dir
+```
+
+4. Configure CMake:
+
+```bash
+mlir build
+cd build
+cmake ../ -DCMAKE_PREFIX_PATH=$(python -m mlir_wheel --root-dir)
+```
+
+5. Build:
+
+```bash
+make -j
+```
---
+++ tutorial/tutorial-opt.cpp
@@ -0,0 +1,27 @@
+//===- tutorial-opt.cpp - Tutorial MLIR Optimizer Driver ------------------===//
+//
+// A simple mlir-opt like tool for the tutorial.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllExtensions.h"
+#include "mlir/InitAllPasses.h"
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
+
+int main(int argc, char **argv) {
+  mlir::DialectRegistry registry;
+
+  // Register all MLIR dialects.
+  mlir::registerAllDialects(registry);
+
+  // Register all MLIR passes.
+  mlir::registerAllPasses();
+
+  // Register all extensions.
+  mlir::registerAllExtensions(registry);
+
+  return mlir::asMainReturnCode(
+      mlir::MlirOptMain(argc, argv, "Tutorial optimizer driver\n", registry));
+}
